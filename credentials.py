7253e7f56f7a7dcb8e32296c3fb19bb0aa8d1cec
Ashima Singhal <57868216+ashimasinghal@users.noreply.github.com>
2020-07-08 17:45:05 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+API_KEY = 'JT8Wa2aYaJoWFuegJCbFW8TCx'
+API_SECRET_KEY = 'kJ41d6SNnoX1HjmOCtstDSCelqEdgOWhoTwJJfeRmjwlofyaKS'
+ACCESS_TOKEN = '1195996094531923968-ylCIuHcmFqeY1rsYO5eugeg6owj3BU'
+ACCESS_TOKEN_SECRET = '2Dnj3h9ZJwXIOxhWLc8gHhLEGom2MawRlM243ItvSqCPL'
+API_KEY_TONE_ANALYZER = 'EJd-WGyCekyD5pJA2bOodqIZ6tiDg5OExRcCo7a9gf18'
+URL_TONE_ANALYZER = 'https://api.eu-gb.tone-analyzer.watson.cloud.ibm.com/instances/3f564a30-979a-41d6-a0ff-e1e753125685'
+VERSION = '2017-09-21'
+
---
+++ settings2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+TRACK_WORDS = ['lockdownindia','coronavirusindia','covidindia','stayhomestaysafe']
+TABLE_NAME = "lockdown"
+TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), polarity INT,\
+subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255),user_description VARCHAR(255), \
+user_followers_count INT, longitude DOUBLE, latitude DOUBLE, retweet_count INT, favorite_count INT"
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
